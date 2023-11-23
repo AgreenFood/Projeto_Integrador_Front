9772bd12bcf8d81fe86824063bef90580b20b0b2
Katia Mendes <katia.mendesx@gmail.com>
2023-11-23 19:13:17 -0300

    atualizando

--- src/components/equipe/Equipe.tsx
+++ src/components/equipe/Equipe.tsx
@@ -87,17 +87,17 @@ export default function Equipe() {
             <h2 className="text-2xl font-poppins font-bold">
               Somos um e-commerce que vende alimentos orgânicos produzidos por pequenos produtores rurais.
             </h2>
-            <p className="font-montserrat margin-20px">
+            <p className="font-montserrat">
               Acreditamos que a agricultura orgânica é uma forma de produção sustentável que beneficia o meio ambiente, os produtores rurais e os consumidores. Os alimentos orgânicos são produzidos sem o uso de agrotóxicos, fertilizantes químicos ou outros insumos artificiais. Isso os torna mais saudáveis para os consumidores e mais seguros para o meio ambiente.
             </p>
-            <p className="font-montserrat margin-20px">
+            <p className="font-montserrat margin-40px">
               A AgeenFood surgiu da necessidade de conectar pequenos produtores rurais com consumidores interessados em alimentos orgânicos. Nosso objetivo é contribuir para o desenvolvimento da agricultura orgânica no Brasil e para o cumprimento do ODS 2 da ONU: acabar com a fome, alcançar a segurança alimentar, melhoria da nutrição e promover a agricultura sustentável.
             </p>
             <h2 className="text-2xl font-poppins font-bold">Como funciona:</h2>
             <p className="font-montserrat margin-20px">
               A AgreenFood trabalha com uma rede de pequenos produtores rurais de todo o Brasil. Esses produtores são selecionados por critérios de qualidade, sustentabilidade e responsabilidade social.
             </p>
-            <p className="font-montserrat margin-20px">
+            <p className="font-montserrat margin-40px">
               Os produtos orgânicos são comercializados a preços acessíveis, para que todas as pessoas tenham acesso a alimentos saudáveis e de alta qualidade.
             </p>
             <h2 className="text-2xl font-poppins font-bold">Faça parte da AgreenFood!</h2>
