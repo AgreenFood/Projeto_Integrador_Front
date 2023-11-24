77e05bbbe5d9f66610627a1db2e9a60750faf24a
Olavo <olavotp@gmail.com>
2023-11-24 09:39:32 -0300

    menu

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -37,7 +37,7 @@ function Navbar() {
                     </svg>
                     </button>
 
-                    {/* <!-- Dropdown menu --> */}
+                    {/* { <!-- Dropdown menu --> } */}
                     <div id="dropdown" className="z-10 hidden bg-android-verde divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                         <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
                             <li>
