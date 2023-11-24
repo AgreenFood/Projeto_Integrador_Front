53e9c163222714060391102b55510be13cc13bef
Elisa Souza <contato.elisasouza@hotmail.com>
2023-11-24 07:09:54 -0300

    finalizando home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,12 @@
 import Carousel from 'react-bootstrap/Carousel';
+import { Link } from 'react-router-dom';
 import './Home.css'
 
 function Home() {
 
     return (
         <>
-            <div className="bg-casca-ovo h-[100vh] home">
+            <div className="home">
 
                 <div>
                     <Carousel fade className=" flex justify-center carousel-image" indicators={false} controls={false}>
@@ -38,50 +39,58 @@ function Home() {
                     </Carousel>
                 </div>
 
-                <div className="flex mx-40 justify-around text-hunter-green">
+                <div className='bg-hunter-green mt-0 flex justify-between'>
+                    <div className='ml-20'>
+                        <Link to='/'>
+                            <img className='w-20' src="https://docs.google.com/uc?id=1t6jWYttF6px5A6m1G5lboD8kPAD3mRgT" alt="" />
+                        </Link>
 
-                    <div className="flex flex-col gap-4 items-center mt-20">
-                        <h2 className="text-5xl font-bold">Bem-vindes ao</h2>
-                        <h2 className="text-5xl font-bold">AGreenFood!</h2>
-                        <p className="text-xl font-montserrat font-bold me-0">Do campo direto para a sua mesa!</p>
-
-                        <div className="flex justify-center">
-                            <img
-                                src="https://docs.google.com/uc?id=1t6jWYttF6px5A6m1G5lboD8kPAD3mRgT
-                                "
-                                alt="Imagem da Home"
-                                width='300px'
-                            />
-                        </div>
                     </div>
 
-                    <div className='w-1/2 mt-10' >
-                        <h3 className='text-justify text-3xl font-poppins font-semibold uppercase mt-2 w-1/2 mx-auto'>Faça parte da nossa família!</h3>
-                        <p className='text-justify text-slate-800 font-montserrat font-semibold mt-2 w-1/2 mx-auto'>
+                    <div className='mr-10 mt-4'>
+                        <ul className='flex justify-between gap-4'>
+                            <li>
+                                <Link to='/login' className='font-montserrat '><button className='bg-verde-leve w-40 h-10 border rounded font-semibold uppercase'>login</button>
+                                </Link>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+
+                <div className='flex justify-center mt-40 mx-auto'></div>
+
+
+                <div className="flex items-center justify-center">
+                    <h1 className="max-w-4xl font-display font-medium tracking-tight text-slate-900 ">
+                        <span className='flex items-center justify-center text-5xl sm:text-7xl flex flex-col gap-4'>
+
+                            <p>Bem-vinde ao</p>
+                            <p className='text-hunter-green'>AGreenfood</p>
+                        </span>
+                        <p className='text-justify text-slate-800 font-montserrat py-2 mt-2 mx-auto w-1/2'>
                             Somos um site onde você, agricultor loca, tem a oportunidade de divulgar seus produtos para um público de maior alcance!
                             Se registrando em nosso site, você poderar cadastrar os produtos que gostaria de vender! O valor para utilizar o site?
                             Somente a sua colaboração em divulgar nosso cantinho verde para que mais pessoas possam ter a oportunidade de fazer parte
                             dessa grande família.
                         </p>
 
-                        <p className='text-justify text-slate-800 font-montserrat font-semibold mt-2 w-1/2 mx-auto'>
+                        <p className='text-justify text-slate-800 font-montserrat mt-2 mx-auto w-1/2'>
                             Na AGreenfood também nos empenhos a alcançar aqueles que mais precisam ser vistos. Temos o compromisso de fazer doações de cestas básicas
                             para pessoas de baixa ou nehuma renda. Todos que gostariam de contribuir com essa missão, podem entrar em contato conosco para mais detalhes! Curtiu nossa proposta? Fala login ou cadastre-se
-
-                            <div className='flex justify-center'>
-                                <button className='flex justify-center mx-auto mt-4 items-center bg-verde-leve w-40 h-10 rounded uppercase font-montserrat font-bold'>
-                                    cadastrar
-                                </button>
-                                <button className='flex justify-center mx-auto mt-4 items-center bg-verde-leve w-40 h-10 rounded uppercase font-montserrat font-bold'>
-                                    login
-                                </button>
-                            </div>
                         </p>
+                        <div className='text-center  mb-10 py-2'>
+                            <Link to='/cadastro'>
+                                <button className='bg-verde-leve hover:bg-android-verde font-poppins mt-2 mx-auto w-1/2 h-10 font-semibold border rounded-lg'>cadastrar</button>
+                            </Link>
+                        </div>
+                    </h1>
+
+                    <div className='flex items-center justify-center'>
+                        <img className='w-1/2' src="https://docs.google.com/uc?id=1t6jWYttF6px5A6m1G5lboD8kPAD3mRgT" alt="" />
                     </div>
-
                 </div>
-            </div >
-
+            </div>
         </>
     )
 }
